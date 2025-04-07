6da23ff8ce29f148eade9d7a9d60f943e300fe04
Misbah Haque <misbahmuneer2003@gmail.com>
2025-04-07 10:49:18 +0500

    Update

--- trackr/lib/Pages/HomePage.dart
+++ trackr/lib/Pages/HomePage.dart
@@ -48,14 +48,14 @@ class _HomePageState extends State<HomePage> {
             Container(
               width: MediaQuery.of(context).size.width,
               margin: EdgeInsets.only(left: 20.0, right: 20.0),
-              height: MediaQuery.of(context).size.height / 2,
+              height: MediaQuery.of(context).size.height / 2.2,
               decoration: BoxDecoration(
                 color: Color(0xff6053f8),
                 borderRadius: BorderRadius.circular(25),
               ),
               child: Column(
                 children: [
-                  SizedBox(height: 20.0),
+                  SizedBox(height: 30.0),
                   Text(
                     "Track your Shipment",
                     style: AppWidget.WhiteTextFieldStyle(),
@@ -65,14 +65,32 @@ class _HomePageState extends State<HomePage> {
                     "Please enter your tracking number",
                     style: AppWidget.DifferentWhiteTextFieldStyle(),
                   ),
-                  SizedBox(height: 30.0),
+                  SizedBox(height: 40.0),
                   Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5.0,
+                      horizontal: 10.0,
+                    ),
                     margin: EdgeInsets.only(left: 20.0, right: 20.0),
-                    decoration: BoxDecoration(color: Colors.white),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                     child: TextField(
-                      decoration: InputDecoration(border: InputBorder.none),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter Tracking Number",
+                        hintStyle: AppWidget.SimpleTextFieldStyle(),
+                        prefixIcon: Icon(
+                          Icons.track_changes_rounded,
+                          color: Colors.red,
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.black, fontSize: 20.0),
                     ),
                   ),
+                  Spacer(),
+                  Image.asset("images/home.png", height: 200),
                 ],
               ),
             ),
