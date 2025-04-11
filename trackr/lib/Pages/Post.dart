76f3aabf3204c9bc706c50e40db4c0b5a09508f1
Misbah Haque <misbahmuneer2003@gmail.com>
2025-04-11 10:09:32 +0500

    Update

--- trackr/lib/Pages/Post.dart
+++ trackr/lib/Pages/Post.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trackr/Services/Widget_Support.dart';
 
 class PostPage extends StatefulWidget {
   const PostPage({super.key});
@@ -12,7 +13,19 @@ class _PostPageState extends State<PostPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff6053f8),
-      body: Container(child: Column(children: [])),
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Add Package",
+                style: AppWidget.WhiteTextFieldStyle(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
