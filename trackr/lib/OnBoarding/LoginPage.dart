29c3947a486611b7f5cd32cd8f5bd54b0c463fbf
Misbah Haque <misbahmuneer2003@gmail.com>
2025-04-14 12:16:48 +0500

    Login Page Done

--- trackr/lib/OnBoarding/LoginPage.dart
+++ trackr/lib/OnBoarding/LoginPage.dart
@@ -74,6 +74,95 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
             ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding: EdgeInsets.only(right: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 60.0),
+            Padding(
+              padding: EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Sign in",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      if (passwordController.text != "" &&
+                          emailController.text != "") {
+                        setState(() {
+                          email = emailController.text.trim();
+                          password = passwordController.text.trim();
+                        });
+                        userLogin();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(
+                              "Please fill the Complete Details",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(60),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          color: Color(0xffea6d35),
+                          borderRadius: BorderRadius.circular(60),
+                        ),
+                        child: Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.white,
+                          size: 36.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account?",
+                  style: TextStyle(fontSize: 23.0, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  " Sign Up",
+                  style: TextStyle(fontSize: 23.0, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           ],
         ),
       ),
